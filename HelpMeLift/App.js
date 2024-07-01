0b72c321abf4b90b43cbfd69cb4fc878b8d0d82d
salmanparvez1@gmail.com <salmanparvez1@gmail.com>
2024-07-01 03:15:32 +0100

    Removed .map() buttons
    - Removed buttons.map and replaced it with FlatList.
    - Also changed the location of exercises and muscles and converted muscles to JSON format.

--- HelpMeLift/App.js
+++ HelpMeLift/App.js
@@ -1,71 +1,56 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Modal, Button } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Modal, useWindowDimensions, SafeAreaView, FlatList } from 'react-native';
+import {buttons, exercises} from "./dataFiles/constants"
 
 export default function App() {
 
-  const buttons = [
-    'Deltoids', 'Biceps', 'Triceps', 'Chest', 'Abdominals', 'Trapezius', 
-    'Latissimus Dorsi', 'Lower Back', 'Glutes', 'Quadriceps', 'Hamstrings', 'Calves', 'Forearms'];
+  const windowHeight = useWindowDimensions().height
+  const windowWidth = useWindowDimensions().width
 
-    const exercises = {
-      Deltoids: ["Overhead Press", "Dumbbell Shoulder Press", "Lateral Raise", "Bent Over Rear Delt Flys"],
-      Biceps: ["Dumbbell Bicep Curl", "Barbell Bicep Curl", "Incline Bicep Curl", "Waiter Curls"],
-      Triceps: ["Tricep Dips", "Tricep Pushdown", "Overhead Tricep Extension", "Skull Crushers"],
-      Chest: ["Bench Press", "Chest Fly", "Push Ups", "Incline Bench Press"],
-      Abdominals: ["Crunches", "Leg Raises", "Plank", "Russian Twists"],
-      Trapezius: ["Shrugs", "Upright Rows", "Face Pulls", "Dumbbell Shrugs"],
-      "Latissimus Dorsi": ["Pull Ups", "Lat Pulldown", "Bent Over Rows", "Single Arm Rows"],
-      "Lower Back": ["Deadlifts", "Back Extensions", "Good Mornings", "Supermans"],
-      Glutes: ["Squats", "Hip Thrusts", "Glute Bridge", "Lunges"],
-      Quadriceps: ["Leg Press", "Leg Extensions", "Squats", "Lunges"],
-      Hamstrings: ["Romanian Deadlifts", "Leg Curls", "Good Mornings", "Glute Ham Raise"],
-      Calves: ["Calf Raises", "Seated Calf Raises", "Calf Press", "Donkey Calf Raises"],
-      Forearms: ["Wrist Curls", "Reverse Wrist Curls", "Farmer's Walk", "Reverse Curls"]
-    };
-  
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [selectedExercises, setSelectedExercises] = useState([]);
-    const [selectedTitle, setSelectedTitle] = useState('');
-  
-    const muscleExercises = (title) => {
-      setSelectedTitle(title);
-      setSelectedExercises(exercises[title]);
-      setIsModalVisible(true);
-    };
-  
-    return (
-      <View style={{flex:1, backgroundColor:"#232023", padding:60}}>
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedExercises, setSelectedExercises] = useState([]);
+  const [selectedTitle, setSelectedTitle] = useState('');
 
-        <ScrollView showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}>
+  const muscleExercises = (title) => {
+    setSelectedTitle(title);
+    setSelectedExercises(exercises[title]);
+    setIsModalVisible(true);
+  };
 
-          <StatusBar backgroundColor={"#333333"} barStyle={"dark-content"}/>
-          <Text style={styles.heading}>HelpMeLift</Text>
+  return (
+    <SafeAreaView style={{flex:1, backgroundColor:"#232023"}}>
+      {/* <ScrollView showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}> */}
+        <StatusBar backgroundColor={"#333333"} barStyle={"dark-content"}/>
 
-          {buttons.map((title, index) => (
-            <TouchableOpacity key={index} style={styles.button} onPress={() => muscleExercises(title)}>
-              <Text style={styles.buttonText}>{title}</Text>
-            </TouchableOpacity>
-          ))}
-  
-          <Modal visible={isModalVisible} onRequestClose={() => setIsModalVisible(false)} animationType="slide">
+        <Text style={styles.heading}>HelpMeLift</Text>
 
-            <View style={{flex: 1, backgroundColor: "#333333", padding: 60}}>
-              <Text style={styles.modalHeading}>{selectedTitle}</Text>
-              {selectedExercises.map((exercise, index) => (
-                <TouchableOpacity key={index} style={styles.button} onPress={() => null}>
-                  <Text style={styles.buttonText}>{exercise}</Text>
-                </TouchableOpacity>
-              ))}
-            </View>
+        <FlatList data = {buttons} ListHeaderComponent={<Text style={styles.heading}>HelpMeLift</Text>} 
+          renderItem={({ item, index }) =>{
+            return(
+              <TouchableOpacity key={index} style={styles.button} onPress={() => muscleExercises(item.Muscle)}>
+                <Text style={styles.buttonText}>{item.Muscle}</Text>
+              </TouchableOpacity>
+            );
+        }}/>
 
-          </Modal>
+        <Modal visible={isModalVisible} onRequestClose={() => setIsModalVisible(false)} animationType="slide">
 
-        </ScrollView>
+          <View style={{flex: 1, backgroundColor: "#333333", padding: 60}}>
+            <FlatList data={selectedExercises} ListHeaderComponent = {<Text style={styles.modalHeading}>{selectedTitle}</Text>} keyExtractor={(item, index) => index.toString()} 
+              renderItem={({ item, index }) => {
+                return(
+                <TouchableOpacity key={index} style={styles.button} onPress={() => muscleExercises(item.Muscle)}>
+                  <Text style={styles.buttonText}>{item}</Text>
+                </TouchableOpacity>)}}/>
+          </View>
 
-      </View>
-    );
-  }
+        </Modal>
+
+      {/* </ScrollView> */}
+    </SafeAreaView>
+  );
+}
   
   const styles = StyleSheet.create({
     heading:{
@@ -76,7 +61,7 @@ export default function App() {
     },
     button: {
       backgroundColor: "crimson",
-      padding: 10,
+      padding: 10.5,
       borderRadius: 5,
       marginVertical: 5,
       alignItems: 'center',
@@ -86,15 +71,11 @@ export default function App() {
       fontSize: 16,
     },
     modalHeading: {
-      color: "white",
+      color: "crimson",
       fontSize: 24,
       marginBottom: 20,
       textAlign: "center"
     },
 
-    modalText: {
-      color: "white",
-      fontSize: 18,
-      marginBottom: 10,
-  }
+    
 });
---
+++ HelpMeLift/dataFiles/constants.js
@@ -0,0 +1,34 @@
+
+
+export const buttons = [
+    { "Muscle":"Deltoids", },
+    { "Muscle": "Biceps", } ,
+    { "Muscle": "Triceps", },
+    { "Muscle": "Chest", },
+    { "Muscle": "Abdominals", }, 
+    { "Muscle": "Trapezius", },
+    { "Muscle": "Latissimus Dorsi", },
+    { "Muscle": "Lower Back", },
+    { "Muscle": "Glutes", },
+    { "Muscle": "Quadriceps", },
+    { "Muscle": "Hamstrings", },
+    { "Muscle": "Calves", },
+    { "Muscle":"Forearms"},
+];
+
+export const exercises = {
+    Deltoids: ["Overhead Press", "Dumbbell Shoulder Press", "Lateral Raise", "Bent Over Rear Delt Flys"],
+    Biceps: ["Dumbbell Bicep Curl", "Barbell Bicep Curl", "Incline Bicep Curl", "Waiter Curls"],
+    Triceps: ["Tricep Dips", "Tricep Pushdown", "Overhead Tricep Extension", "Skull Crushers"],
+    Chest: ["Bench Press", "Chest Fly", "Push Ups", "Incline Bench Press"],
+    Abdominals: ["Crunches", "Leg Raises", "Plank", "Russian Twists"],
+    Trapezius: ["Shrugs", "Upright Rows", "Face Pulls", "Dumbbell Shrugs"],
+    "Latissimus Dorsi": ["Pull Ups", "Lat Pulldown", "Bent Over Rows", "Single Arm Rows"],
+    "Lower Back": ["Deadlifts", "Back Extensions", "Good Mornings", "Supermans"],
+    Glutes: ["Squats", "Hip Thrusts", "Glute Bridge", "Lunges"],
+    Quadriceps: ["Leg Press", "Leg Extensions", "Squats", "Lunges"],
+    Hamstrings: ["Romanian Deadlifts", "Leg Curls", "Good Mornings", "Glute Ham Raise"],
+    Calves: ["Calf Raises", "Seated Calf Raises", "Calf Press", "Donkey Calf Raises"],
+    Forearms: ["Wrist Curls", "Reverse Wrist Curls", "Farmer's Walk", "Reverse Curls"]
+}
+
