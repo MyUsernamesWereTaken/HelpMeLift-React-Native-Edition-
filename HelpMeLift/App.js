052a74d2488c465958948229f82f74dd27c8c400
salmanparvez1@gmail.com <salmanparvez1@gmail.com>
2024-07-12 04:04:30 +0100

    Tabs Created
    Fill in Screens and Develop Muscle Group further

--- HelpMeLift/App.js
+++ HelpMeLift/App.js
@@ -1,81 +1,29 @@
-import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Modal, useWindowDimensions, SafeAreaView, FlatList } from 'react-native';
-import {buttons, exercises} from "./dataFiles/constants"
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "./screens/Home";
 
-export default function App() {
 
-  const windowHeight = useWindowDimensions().height
-  const windowWidth = useWindowDimensions().width
 
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [selectedExercises, setSelectedExercises] = useState([]);
-  const [selectedTitle, setSelectedTitle] = useState('');
 
-  const muscleExercises = (title) => {
-    setSelectedTitle(title);
-    setSelectedExercises(exercises[title]);
-    setIsModalVisible(true);
-  };
+const Tab = createBottomTabNavigator();
 
-  return (
-    <SafeAreaView style={{flex:1, backgroundColor:"#232023"}}>
-      {/* <ScrollView showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}> */}
-        <StatusBar backgroundColor={"#333333"} barStyle={"dark-content"}/>
+export default function App(){
 
-        <Text style={styles.heading}>HelpMeLift</Text>
+  return(
 
-        <FlatList data = {buttons} ListHeaderComponent={<Text style={styles.heading}>HelpMeLift</Text>} 
-          renderItem={({ item, index }) =>{
-            return(
-              <TouchableOpacity key={index} style={styles.button} onPress={() => muscleExercises(item.Muscle)}>
-                <Text style={styles.buttonText}>{item.Muscle}</Text>
-              </TouchableOpacity>
-            );
-        }}/>
+    <NavigationContainer>
 
-        <Modal visible={isModalVisible} onRequestClose={() => setIsModalVisible(false)} animationType="slide">
+      <Tab.Navigator>
 
-          <View style={{flex: 1, backgroundColor: "#333333", padding: 60}}>
-            <FlatList data={selectedExercises} ListHeaderComponent = {<Text style={styles.modalHeading}>{selectedTitle}</Text>} keyExtractor={(item, index) => index.toString()} 
-              renderItem={({ item, index }) => {
-                return(
-                <TouchableOpacity key={index} style={styles.button} onPress={() => muscleExercises(item.Muscle)}>
-                  <Text style={styles.buttonText}>{item}</Text>
-                </TouchableOpacity>)}}/>
-          </View>
+        <Tab.Screen name="Home" component={Home}/>
+        <Tab.Screen name="Journal" component={Journal}/>
+        <Tab.Screen name="My Plan" component={Plan}/>
+        <Tab.Screen name="Profile" component={Profile}/>
 
-        </Modal>
+      </Tab.Navigator>
 
-      {/* </ScrollView> */}
-    </SafeAreaView>
+    </NavigationContainer>
+
   );
-}
-  
-  const styles = StyleSheet.create({
-    heading:{
-      color: "crimson",
-      fontSize: 24,
-      marginBottom: 20,
-      textAlign: "center",
-    },
-    button: {
-      backgroundColor: "crimson",
-      padding: 10.5,
-      borderRadius: 5,
-      marginVertical: 5,
-      alignItems: 'center',
-    },
-    buttonText: {
-      color: "#232023",
-      fontSize: 16,
-    },
-    modalHeading: {
-      color: "crimson",
-      fontSize: 24,
-      marginBottom: 20,
-      textAlign: "center"
-    },
 
-    
-});
+}
--- HelpMeLift/dataFiles/constants.js
+++ HelpMeLift/dataFiles/constants.js
@@ -1,5 +1,3 @@
-
-
 export const buttons = [
     { "Muscle":"Deltoids", },
     { "Muscle": "Biceps", } ,
