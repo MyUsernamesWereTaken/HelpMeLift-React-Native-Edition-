2dfdee64f99dbf82b33a54f01d7d9620eb0d0317
salmanparvez1@gmail.com <salmanparvez1@gmail.com>
2024-07-22 17:41:25 +0100

    Revert "Nested Stack and Tab Navigations"
    This reverts commit 9385e4398dd78e8ef035c3dba00dc715aade0ae2.

--- HelpMeLift/App.js
+++ HelpMeLift/App.js
@@ -1,21 +1,25 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Home from "./Screens/HomeStack/Home";
-import Journal from "./Screens/Journal";
-import Plan from "./Screens/Plan";
-import Profile from "./Screens/Profile";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import { InitialStack } from "./Stack";
+import Home from "./screens/Home";
+import Journal from "./screens/Journal"
+import Plan from "./screens/Plan"
+import Profile from "./screens/Profile";
+import Ionicons from "@expo/vector-icons/Ionicons"
+
+
 
 const Tab = createBottomTabNavigator();
 
-export default function App() {
-  return (
+export default function App(){
+
+  return(
+
     <NavigationContainer>
+
       <Tab.Navigator
         screenOptions={({ route }) => ({
-          headerShown: false,
-          tabBarStyle: { backgroundColor: "#000000" },
+          headerShown:false,
+          tabBarStyle: { backgroundColor: "#000000"},
           tabBarInactiveTintColor: "red",
           tabBarActiveTintColor: "#51A9C3",
           tabBarIcon: ({ focused, color }) => {
@@ -31,14 +35,16 @@ export default function App() {
             }
             return <Ionicons name={iconName} size={25} color={color} />;
           },
-        })}
-      >
-        <Tab.Screen name="Home" component={InitialStack} />
-        <Tab.Screen name="Journal" component={Journal} />
-        <Tab.Screen name="My Plan" component={Plan} />
-        <Tab.Screen name="Profile" component={Profile} />
-        
+        })}>
+
+        <Tab.Screen name="Home" component={Home}/>
+        <Tab.Screen name="Journal" component={Journal}/>
+        <Tab.Screen name="My Plan" component={Plan}/>
+        <Tab.Screen name="Profile" component={Profile}/>
+
       </Tab.Navigator>
+
     </NavigationContainer>
+
   );
 }
